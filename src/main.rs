9a0334f7f76beab917827df71c151f18b2775a51
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-07-11 21:12:49 +0200

    feat: write "Hello, world!" to the screen

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,19 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let buf = 0xb8000 as *mut u8;
+    static HELLO_WORLD: &[u8] = b"Hello, world!";
+
+    // Loop through each character
+    for (i, &b) in HELLO_WORLD.iter().enumerate() {
+        unsafe {
+            // Print character
+            *buf.offset(i as isize * 2) = b;
+
+            // 0x0 = VGA color black
+            *buf.offset(i as isize * 2 + 1) = 0x0;
+        }
+    }
+
     loop {}
 }
