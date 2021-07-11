2f3320f95f350a9a8e8f078a8937e03a39da6f8f
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-07-12 00:45:35 +0200

    feat: power cycle after 3 seconds

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,22 @@
 #![feature(abi_efiapi)]
 
 extern crate rlibc;
+
 use core::fmt::Write;
 use uefi::prelude::*;
+use uefi::table::runtime::ResetType;
 
 #[entry]
 fn main(_image: Handle, st: SystemTable<Boot>) -> Status {
+    let bs = st.boot_services();
+    let rs = st.runtime_services();
+
     st.stdout().reset(true).unwrap().unwrap();
     st.stdout().write_str("Hello, world!\n").unwrap();
-    loop {}
+
+    bs.stall(3000 * 1000);
+
+    rs.reset(ResetType::Cold, Status::SUCCESS, None);
 }
 
 #[panic_handler]
